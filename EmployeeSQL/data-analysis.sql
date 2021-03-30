1a12ffc6a12896601c876be5dc843642cf24462d
tnguyen0306 <tnguyen0306@gmail.com>
2021-03-30 15:42:22 -0500

    Beta 1.5.0

--- EmployeeSQL/data-analysis.sql
+++ EmployeeSQL/data-analysis.sql
@@ -1,37 +1,37 @@
 --List the following details of each employee: employee number, last name, first name, sex, and salary.
-SELECT 
-	e.emp_no,
-	e.last_name,
-	e.first_name,
-	e.sex,
-	s.salary
+SELECT e.emp_no,
+	   e.last_name,
+	   e.first_name,
+	   e.sex,
+	   s.salary
 FROM employees as e
 JOIN salaries as s
 ON s.emp_no = e.emp_no
 ORDER BY e.emp_no;
 
+
 --List first name, last name, and hire date for employees who were hired in 1986.
-SELECT 
-	first_name,
-	last_name,
-	hire_date
+SELECT first_name,
+	   last_name,
+	   hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
 ORDER BY hire_date;
 
+
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-SELECT 
-	d.dept_no, 
-	d.dept_name, 
-	dm.emp_no, 
-	e.last_name, 
-	e.first_name 
+SELECT d.dept_no, 
+	   d.dept_name, 
+	   dm.emp_no, 
+	   e.last_name, 
+	   e.first_name 
 FROM departments as d
 JOIN dept_manager as dm
 ON d.dept_no = dm.dept_no
 JOIN employees as e
 ON dm.emp_no = e.emp_no
 ORDER BY d.dept_no;
+
 
 --List the department of each employee with the following information: employee number, last name, first name, and department name.
 SELECT 
@@ -47,7 +47,11 @@ ON d.dept_no = de.dept_no
 ORDER BY e.emp_no;
 
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
+SELECT first_name, 
+	   last_name
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
 
 --List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
