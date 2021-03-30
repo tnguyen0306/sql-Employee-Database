3660150c2973dbbd05a7f7bfa1a32e60624d8f60
tnguyen0306 <tnguyen0306@gmail.com>
2021-03-30 15:54:11 -0500

    Ver 1.0.0

--- EmployeeSQL/data-analysis.sql
+++ EmployeeSQL/data-analysis.sql
@@ -84,7 +84,7 @@ ORDER BY e.emp_no;
 
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-SELECT last_name AS "Last Name",
+SELECT last_name,
 	   COUNT(last_name) AS "Frequency Count"
 FROM employees
 GROUP BY last_name
