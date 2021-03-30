6b86d540bfe77e557a0127effbf65c9dffa3c7c4
tnguyen0306 <tnguyen0306@gmail.com>
2021-03-30 15:46:51 -0500

    Beta 1.7.0

--- EmployeeSQL/data-analysis.sql
+++ EmployeeSQL/data-analysis.sql
@@ -66,6 +66,17 @@ WHERE d.dept_name = 'Sales'
 ORDER BY e.emp_no;
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+SELECT e.emp_no, 
+	   e.last_name, 
+	   e.first_name,
+	   d.dept_name
+FROM employees as e
+JOIN dept_emp as de
+ON de.emp_no = e.emp_no
+JOIN departments as d
+ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'Sales'
+OR d.dept_name = 'Development'
+ORDER BY e.emp_no;
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
