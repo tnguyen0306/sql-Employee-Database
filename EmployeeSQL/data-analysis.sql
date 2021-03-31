2eed06ca92011dec1c649f3bf4969f5cbb261b1a
tnguyen0306 <tnguyen0306@gmail.com>
2021-03-30 21:43:46 -0500

    Ver 1.0.1

--- EmployeeSQL/data-analysis.sql
+++ EmployeeSQL/data-analysis.sql
@@ -48,7 +48,8 @@ ORDER BY e.emp_no;
 
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, 
-	   last_name
+	   last_name,
+	   sex
 FROM employees
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
